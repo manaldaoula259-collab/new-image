efe83d6070d0a8a28d3b78f3d0484e2e31c13628
manaldaoula259-collab <manaldaoula259@gmail.com>
2026-01-20 15:19:35 +0100

    Update page.tsx

--- src/app/(public)/ai-image-editor/remove-object-from-image/page.tsx
+++ src/app/(public)/ai-image-editor/remove-object-from-image/page.tsx
@@ -6,7 +6,7 @@ export const dynamic = "force-dynamic";
 
 
 export const metadata: Metadata = {
-  title: "Remove Object from Image - AI Image Editor | MyDzine",
+  title: "Remove Object from Image - AI Image Editor | poweraitool",
   description:
     "Effortlessly remove unwanted objects from your images with AI-powered editing. Designed for all skill levels to unlock creative potential in seconds!",
 };
