6091db8d53ebeaeb2248d412f8f4d1245bd1db58
manaldaoula259-collab <manaldaoula259@gmail.com>
2026-01-20 15:23:01 +0100

    Update page.tsx

--- src/app/(public)/privacy/page.tsx
+++ src/app/(public)/privacy/page.tsx
@@ -2,7 +2,7 @@ import PrivacyPage from "@/components/pages/PrivacyPage";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Privacy Policy | MyDzine",
+  title: "Privacy Policy | poweraitool",
 };
 
 export const dynamic = "force-dynamic";
