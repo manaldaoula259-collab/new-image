abd5c2aa5e70065ce45264fb5bd01d2486bf83b1
manaldaoula259-collab <manaldaoula259@gmail.com>
2026-01-20 15:15:48 +0100

    Update page.tsx

--- src/app/(public)/ai-art-generator/ai-coloring-book-generator/page.tsx
+++ src/app/(public)/ai-art-generator/ai-coloring-book-generator/page.tsx
@@ -7,9 +7,9 @@ export const dynamic = "force-dynamic";
 
 
 export const metadata: Metadata = {
-  title: "AI Coloring Book Generator | MyDzine",
+  title: "AI Coloring Book Generator | poweraitool",
   description:
-    "Upload photos or enter text to create any coloring page you (or your child) can imagine with Dzine AI coloring book generator. Now, transform your ideas into coloring books with just a few clicks.",
+    "Upload photos or enter text to create any coloring page you (or your child) can imagine with poweraitool AI coloring book generator. Now, transform your ideas into coloring books with just a few clicks.",
 };
 
 const AIColoringBookGenerator = () => {
